ca584e2d9b97f3456628702f83474cdd9265f41d
Lynn Langit <lynnlangit@outlook.com>
2014-04-16 09:33:16 -0700

    Initial Commit
    for YouTube Demo

---
+++ BizVerifyDemo/BizVerifyDemo/BizData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using DnB;
+
+namespace BizVerifyDemo
+{
+    class BizVerifyData
+    {
+        //your Live ID <email> example format shown below
+        private const string USER_ID = "me@live.com";
+
+        //your API key -from 'Primary Account Key' in Windows Azure Marketplace>Explore this Dataset,
+        //your key will contain more characters than the example below
+        private const string SECURE_ACCOUNT_ID = "xyzaaaaaaaaaaaa";
+
+        //endpoint for D&B Business Verification API in Windows Azure Marketplace
+        private const string ROOT_SERVICE_URL = 
+            "https://api.datamarket.azure.com/DNB/BusinessVerificationOffice/v1/CompanyMatchByNameAddress";
+
+        public static string bizName = "Microsoft";
+        public static string country = "US";
+        public static string state = "WA";
+
+        private readonly Uri serviceUri;
+        private readonly DnContainer context;
+
+        // constructor
+        public BizVerifyData()
+        {
+            serviceUri = new Uri(ROOT_SERVICE_URL);
+            context = new DnContainer(serviceUri);
+            context.IgnoreMissingProperties = true;
+            context.Credentials = new NetworkCredential(USER_ID,
+                                                        SECURE_ACCOUNT_ID);
+        }
+
+        // the method that queries the service and returns the IList<> of bizVerify data
+        public IList<CleanseMatchByNameAddressEntity> CompanyMatchByNameAddress()
+        {
+            //Calling the exposed method, pass the required parameters in at minimum
+            IEnumerable<CleanseMatchByNameAddressEntity> bizQuery
+                = context.CompanyMatchByNameAddress(bizName, null, null, state, null, country, null, null);
+
+            try
+            {
+                return bizQuery.ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("BizVerify ERROR {0}", ex.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ BizVerifyDemo/BizVerifyDemo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using DnB;
+using BizVerifyDemo;
+
+namespace BizVerifyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<CleanseMatchByNameAddressEntity> businessList = null;
+            var bizData = new BizVerifyData();
+           businessList = bizData.CompanyMatchByNameAddress();
+
+            //============================================ BIZ VERIFY DATA OUTPUT
+            // confirm data was returned from query
+            if (businessList != null)
+            {
+                //show input
+                Console.WriteLine("BizData Input: BizName: " +
+                    BizVerifyData.bizName + " BizState: " + BizVerifyData.state + " BizCountry: " +BizVerifyData.country);
+                Console.Write("Tap to continue");
+                Console.ReadLine();
+
+                // print output column headings
+                Console.WriteLine("Verified Biz Data");
+                Console.WriteLine("{0,2},{1,2},{2,2},{3,2},{4,2},{5,2},{6,2},{7,1},{8,2}", "DUNS", "Company Name",
+                                                                "Street Address",
+                                                                "City",
+                                                                "State",
+                                                                "County",
+                                                               
+                                                                "Zip",
+                                                                "Confidence Code",
+                                                                "Match Grade");
+                // print query results
+                foreach (CleanseMatchByNameAddressEntity entity in businessList)
+                    Console.WriteLine("{0,2},{1,2},{2,2},{3,2},{4,2},{5,2},{6,2},{7,1},{8,2}", entity.DUNSNumber,
+                        entity.Company,
+                                                                    entity.Address,
+                                                                    entity.City,
+                                                                    entity.State,
+                                                                    entity.CountryISOCode,
+                                                                    
+                                                                    entity.ZipCode,
+                                                                    entity.ConfidenceCode,
+                                                                    entity.MatchGrade);
+            }
+            // pause until a key is struck
+            Console.Write("Tap any key to end. ");
+            Console.ReadKey();
+            Console.WriteLine();
+        }
+    }
+}
